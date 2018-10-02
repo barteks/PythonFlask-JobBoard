8807fd78de833dc4da84434673ab6a55f44c0bfd
barteks <bartlomiejszydlowski@gmail.com>
2018-10-01 22:53:00 -0400

    db and macros

--- jobs/app.py
+++ jobs/app.py
@@ -5,12 +5,14 @@ PATH = "db/jobs.sqlite"
 
 app = Flask(__name__)
 
+
 def open_connection():
     connection = getattr(g, "_connection", None)
     if connection == None:
         connection = g._connection = sqlite3.connect(PATH)
     connection.row_factory = sqlite3.Row
     return connection
+
 
 def execute_sql(sql, values=(), commit=False, single=False):
     connection = open_connection()
@@ -22,6 +24,7 @@ def execute_sql(sql, values=(), commit=False, single=False):
 
     cursor.close()
     return results
+
 
 @app.teardown_appcontext
 def close_connection(exeception):
@@ -35,3 +38,6 @@ def close_connection(exeception):
 def jobs():
     jobs = execute_sql("SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id")
     return render_template("index.html", jobs=jobs)
+
+def job():
+    return render_template("job.html")
